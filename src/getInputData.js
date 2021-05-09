efefb8f0a584229a70ae18cea0e4505e86db54b0
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-05-09 20:18:03 +0300

    feat: add add input data parsing method

---
+++ src/getInputData.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const { pipeline } = require('stream');
+
+const cipher = require('./cipher');
+const consoleArgs = require('./getConsoleArgs');
+const errorHandler = require('./errorHandler');
+
+const getInput = (path) => {
+    let readerStream;
+
+    if (path === undefined) {
+        readerStream = process.stdin;
+    } else {
+        fs.access(path, fs.F_OK, (err) => {
+            if (err) {
+                errorHandler(err);
+            }
+        });
+
+        readerStream = fs.createReadStream(path);
+    }
+
+    readerStream.setEncoding('utf-8');
+    const chunks = [];
+
+    return new Promise((resolve, reject) => {
+        readerStream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
+        readerStream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
+    })
+};
+
+module.exports = getInput;
